b51c9d798b4cd1161f0427a18b9eca64dcd8fd5d
inamuu <8310973+inamuu@users.noreply.github.com>
2019-02-20 15:15:45 +0900

    :elephant: 勤怠をポチッとな

--- README.md
+++ README.md
@@ -8,3 +8,20 @@ $ pip3 install -r requirementes.txt
 
 下記からchromedriverをダウンロードして、Applicationフォルダーに配置する。
 https://sites.google.com/a/chromium.org/chromedriver/downloads
+
+## 認証情報をenvchainでセットする
+
+```sh
+envchain --set JOBCAN JOBCANEMAIL
+```
+```sh
+envchain --set JOBCAN JOBCANPASSWORD
+```
+
+## 実行方法
+
+envchainで環境変数を呼び出して実行する
+
+```sh
+envchain JOBCAN python3 main.py
+```
--- main.py
+++ main.py
@@ -1,15 +1,21 @@
+import os
 import time
 from selenium import webdriver
 
 driver = webdriver.Chrome(executable_path='/Applications/chromedriver')
 
 def main():
-    driver.get('https://www.google.com/')
-    time.sleep(3)
-    search_box = driver.find_element_by_name("q")
-    search_box.send_keys('ChromeDriver')
-    search_box.submit()
-    time.sleep(3)
+    driver.get('https://id.jobcan.jp/users/sign_in?app_key=atd&redirect_to=https://ssl.jobcan.jp/jbcoauth/callback')
+    time.sleep(1)
+    id = driver.find_element_by_id("user_email")
+    id.send_keys(os.environ['JOBCANEMAIL'])
+    password = driver.find_element_by_id("user_password")
+    password.send_keys(os.environ['JOBCANPASSWORD'])
+    element = driver.find_element_by_name("commit")
+    element.click()
+    push = driver.find_element_by_name("adit_item")
+    push.click()
+    time.sleep(1)
     driver.quit()
 
 if __name__ == "__main__": main()
