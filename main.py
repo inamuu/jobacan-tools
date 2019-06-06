8ba8d76c6d41995804781b77056f494c52d5c260
inamuu <8310973+inamuu@users.noreply.github.com>
2019-06-07 01:08:22 +0900

    :+1: 打刻エラーのチェックができるところまで

--- main.py
+++ main.py
@@ -15,29 +15,30 @@ load_dotenv(dotenv_path)
 MAILADDR = os.environ.get('JOBCANEMAIL')
 LOGINPASS = os.environ.get('JOBCANPASSWORD')
 
-driver = webdriver.Chrome(executable_path='/Applications/chromedriver')
-driver.get('https://id.jobcan.jp/users/sign_in?app_key=atd&redirect_to=https://ssl.jobcan.jp/jbcoauth/callback')
-
 @click.group()
 def subcommand():
     pass
 
 def jobcanlogin():
+    driver = webdriver.Chrome(executable_path='/Applications/chromedriver')
+    driver.get('https://id.jobcan.jp/users/sign_in?app_key=atd&redirect_to=https://ssl.jobcan.jp/jbcoauth/callback')
+
+    if 'MAILADDR' is None or 'LOGINPASS' is None:
+      print('認証情報をセットしてください。')
+      return
+
     id = driver.find_element_by_id("user_email")
     id.send_keys(MAILADDR)
     password = driver.find_element_by_id("user_password")
     password.send_keys(LOGINPASS)
     element = driver.find_element_by_name("commit")
     element.click()
+    return driver
 
 @subcommand.command(help='打刻')
 def touch():
-    if 'MAILADDR' is None or 'LOGINPASS' is None:
-      print('認証情報をセットしてください。')
-      return
-
     try:
-        jobcanlogin()
+        driver = jobcanlogin()
         status = driver.find_element_by_id('working_status').text
         if status == '退室中':
             push = driver.find_element_by_name("adit_item")
@@ -45,8 +46,21 @@ def touch():
             print('打刻しました')
         else:
             print('既に打刻済みです')
-   except:
+    except:
         print('打刻に失敗しました')
+    driver.quit()
+    return
+
+@subcommand.command(help='打刻エラーチェック')
+def checkerr():
+    print('打刻エラーをチェックします')
+    try:
+        driver = jobcanlogin()
+        time.sleep(5)
+        tableid = driver.find_element_by_id('top_info_area').text
+        print(tableid)
+    except:
+        print('エラーチェックに失敗しました')
     driver.quit()
     return
 
